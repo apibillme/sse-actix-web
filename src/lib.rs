78ba667a3167e3f88aaf8199a5af8509ac0ae697
Bevan Hunt <bevan@bevanhunt.com>
2019-12-20 18:59:23 +0000

    add allow all origin CORS

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub async fn new_client(broadcaster: Data<Mutex<Broadcaster>>) -> impl Responder
 
     HttpResponse::Ok()
         .header("content-type", "text/event-stream")
+        .header("Access-Control-Allow-Origin",  "*")
         .no_chunking()
         .streaming(rx)
 }
